959718b573418d867c4b20dde705c797719147aa
Tiago Garcia <tiagoromerogarcia@gmail.com>
2011-05-03 22:04:14 -0700

    Done

---
+++ task1/scripts.js
@@ -0,0 +1,20 @@
+function keyPressed(event)
+{
+	var code, arrow;
+	if (!event) var event = window.event;
+	if (event.keyCode) code = event.keyCode;
+	else if (event.which) code = event.which;
+	
+	switch (code)
+	{
+		case 37: arrow = 'left'; break;
+		case 38: arrow = 'up'; break;
+		case 39: arrow = 'right'; break;
+		case 40: arrow = 'down'; break;
+	}
+	
+	if (arrow !== undefined)
+	{
+		document.body.innerHTML += 'You pressed the ' + arrow + ' arrow.<br>';	
+	}
+}
